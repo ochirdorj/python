09af068f68d0ba81d9d8f10c20ea2b8572b54f4c
ochirdorj <erdenetugs@gmail.com>
2025-06-05 03:43:57 +0000

    new_files

---
+++ classtask/grade.py
@@ -0,0 +1,24 @@
+grade = int(input("Please provide your grade: "))
+
+# 95
+
+if grade <= 100 and grade >= 90:
+    print("grade is A")
+if grade <= 89 and grade >= 80:
+    print("grade is B")
+if grade <= 79 and grade >= 70:
+    print("grade is C")
+if grade <= 69 and grade >= 60:
+    print("grade is D")
+if grade <= 59 and grade >= 0:
+    print("grade is F")
+if grade > 100 or grade < 0:
+    print("invalid input")
+
+
+
+# A = 90 <= 100
+# B = 80 <= 89
+# C = 70 <= 79
+# D 
+
