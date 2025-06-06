04353113812efcfd204157b5e6b4333fe7c0ed82
ochirdorj <erdenetugs@gmail.com>
2025-06-06 16:36:46 +0000

    up

---
+++ hometask/session_2/taks1.py
@@ -0,0 +1,15 @@
+# Task 1: Number info
+# Description:
+#  Ask the user to enter a number. Your program should:
+# Check if the number is positive, negative, or zero
+# Check if the number is even or odd
+# Print both results
+# Example:
+# Input: -11  
+# Output:
+# The number is negative  
+# # The number is odd
+
+number = int(input("Please provide me the number you want to check:" ))
+
+if number = 
